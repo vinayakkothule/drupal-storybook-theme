280aaa243b5a6918f2d396c24963e8d78fbb67c8
Vinayak Kothule <vkothule0965@gmail.com>
2025-08-18 21:13:02 +0530

    Added tailwind config files
    - CSS files source & destination
    - Tailwind config files
    - Postcss config files

---
+++ web/themes/custom/tailpine/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html.twig",
+    "./templates/**/*.html.twig",
+    "./components/**/*.twig",
+    "./components/**/*.stories.json",
+    "./components/**/*.stories.yml",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
