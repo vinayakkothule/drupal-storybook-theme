3de9d6641c33fbab6dcd7db43b87399d0315e2ea
Vinayak Kothule <vkothule0965@gmail.com>
2025-08-18 20:45:51 +0530

    New custom theme is added

---
+++ web/themes/custom/tailpine/js/tailpine.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * tailpine behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.tailpine = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
